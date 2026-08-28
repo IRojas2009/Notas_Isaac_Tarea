76eaf714c698c6a756398fe687f62137b6247b9d
IRojas2009 <isaacrv060909@gmail.com>
2026-08-28 08:40:22 -0600

    3. Aspecto Principal

--- app/src/main/java/com/my/first/notas_isaac/MainActivity.kt
+++ app/src/main/java/com/my/first/notas_isaac/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.my.first.notas_isaac
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.my.first.notas_isaac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.fabAddNote.setOnClickListener {
+            Toast.makeText(applicationContext,
+                "Has presionado en el FAB",
+            Toast.LENGTH_SHORT).show()
+
         }
     }
 }
