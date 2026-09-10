adc026661c767f3f2734aea00dedc645f53dc680
IRojas2009 <isaacrv060909@gmail.com>
2026-09-10 16:52:20 -0600

    12. Actualizar Nota UI

--- app/src/main/java/com/my/first/notas_isaac/NotasAdaptador.kt
+++ app/src/main/java/com/my/first/notas_isaac/NotasAdaptador.kt
@@ -1,7 +1,10 @@
 package com.my.first.notas_isaac
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.my.first.notas_isaac.databinding.ItemNotaBinding
@@ -10,7 +13,9 @@ class NotasAdaptador(
     private var notas: List<Nota>,
 ) : RecyclerView.Adapter<NotasAdaptador.NotaViewHolder>() {
 
-    class NotaViewHolder(val binding: ItemNotaBinding) : RecyclerView.ViewHolder(binding.root)
+    class NotaViewHolder(val binding: ItemNotaBinding) : RecyclerView.ViewHolder(binding.root){
+        val ivActualizar : ImageView = itemView.findViewById(R.id.ivActualizar)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
         val binding = ItemNotaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -23,6 +28,19 @@ class NotasAdaptador(
         val nota = notas[position]
         holder.binding.itemTitulo.text = nota.titulo
         holder.binding.itemDescripcion.text = nota.descripcion
+
+        holder.ivActualizar.setOnClickListener {
+            val intent = Intent(holder.itemView.context, ActualizarNotaActivity::class.java).apply {
+                putExtra("id_nota", nota.id)
+            }
+
+            holder.itemView.context.startActivity(intent)
+            Toast.makeText(
+                holder.itemView.context,
+                "El id de la nota seleccionada es ${nota.id}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     fun refreshData(newNotas: List<Nota>) {
